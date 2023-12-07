07162d8533297680f1e55715dfdd33fffa10c2f4
sirfoxpoof <daniquedrost4@gmail.com>
2023-12-07 15:56:33 +0100

    Prefabs voor bomen + nieuw font

---
+++ Assets/ProjectFolder/Scripts/UIScripts/MenuFuntions/VolumeSet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSet : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer masterMixer, musicMixer, sfxMixer;
+
+
+}
