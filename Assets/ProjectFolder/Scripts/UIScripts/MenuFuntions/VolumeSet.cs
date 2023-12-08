4fc46722de568ee25da7f2da240dd956c760cb8e
sirfoxpoof <daniquedrost4@gmail.com>
2023-12-08 21:39:18 +0100

    Skybox fix + sounds + LOD setup
    Different skybox
    Orbs have a sparkle sound
    Background ambience + soft music
    LOD setup for the trees + stones

--- Assets/ProjectFolder/Scripts/UIScripts/MenuFuntions/VolumeSet.cs
+++ Assets/ProjectFolder/Scripts/UIScripts/MenuFuntions/VolumeSet.cs
@@ -2,11 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
 public class VolumeSet : MonoBehaviour
 {
     [SerializeField]
-    private AudioMixer masterMixer, musicMixer, sfxMixer;
+    private AudioMixer volumeMixer;
+    private Slider masterSlider, musicSlider, sfxSlider;
+
+    public void SetMasterVolume(float sliderValue)
+    {
+        volumeMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MasterVolumeSave", masterSlider.value);
+    }
+
+    public void SetMusicVolume(float sliderValue)
+    {
+        volumeMixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MasterVolumeSave", musicSlider.value);
+    }
+
+
+    public void SetSFXVolume(float sliderValue)
+    {
+        volumeMixer.SetFloat("SoundEffectVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MasterVolumeSave", sfxSlider.value);
+    }
+
 
 
 }
