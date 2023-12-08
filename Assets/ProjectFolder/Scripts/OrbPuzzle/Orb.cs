13d46bf325dc3c39970776ebfc9dd496770867fd
Doetzen <Doutzenhoekstra06@gmail.com>
2023-12-08 14:08:17 +0100

    OrbPuzzle werkt

--- Assets/ProjectFolder/Scripts/OrbPuzzle/Orb.cs
+++ Assets/ProjectFolder/Scripts/OrbPuzzle/Orb.cs
@@ -6,7 +6,7 @@ public class Orb : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] pedestalPrefabs;
-
+    [SerializeField]
     private bool winOrbPuzzle;
 
     private void Update()
