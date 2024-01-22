bb4edf9b0ad776ca83a7f82bd0a4983be5d1783d
sirfoxpoof <daniquedrost4@gmail.com>
2024-01-22 16:47:53 +0100

    UI updates
    In game settingsmenu wip
    sound settings die nu wel werken ha

--- Assets/ProjectFolder/InputSystem/InputAtionMap.cs
+++ Assets/ProjectFolder/InputSystem/InputAtionMap.cs
@@ -53,6 +53,15 @@ public partial class @InputAtionMap: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""MenuPop"",
+                    ""type"": ""Button"",
+                    ""id"": ""89b6deaa-831d-4a4d-a540-0f1a1a530210"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -88,6 +97,17 @@ public partial class @InputAtionMap: IInputActionCollection2, IDisposable
                     ""action"": ""Jump"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""09092c76-9523-4503-827e-65ad5df07ca3"",
+                    ""path"": ""<XRController>{LeftHand}/{MenuButton}"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MenuPop"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -99,6 +119,7 @@ public partial class @InputAtionMap: IInputActionCollection2, IDisposable
         m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
         m_Player_Rotate = m_Player.FindAction("Rotate", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_MenuPop = m_Player.FindAction("MenuPop", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -163,6 +184,7 @@ public partial class @InputAtionMap: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Movement;
     private readonly InputAction m_Player_Rotate;
     private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_MenuPop;
     public struct PlayerActions
     {
         private @InputAtionMap m_Wrapper;
@@ -170,6 +192,7 @@ public partial class @InputAtionMap: IInputActionCollection2, IDisposable
         public InputAction @Movement => m_Wrapper.m_Player_Movement;
         public InputAction @Rotate => m_Wrapper.m_Player_Rotate;
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @MenuPop => m_Wrapper.m_Player_MenuPop;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -188,6 +211,9 @@ public partial class @InputAtionMap: IInputActionCollection2, IDisposable
             @Jump.started += instance.OnJump;
             @Jump.performed += instance.OnJump;
             @Jump.canceled += instance.OnJump;
+            @MenuPop.started += instance.OnMenuPop;
+            @MenuPop.performed += instance.OnMenuPop;
+            @MenuPop.canceled += instance.OnMenuPop;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -201,6 +227,9 @@ public partial class @InputAtionMap: IInputActionCollection2, IDisposable
             @Jump.started -= instance.OnJump;
             @Jump.performed -= instance.OnJump;
             @Jump.canceled -= instance.OnJump;
+            @MenuPop.started -= instance.OnMenuPop;
+            @MenuPop.performed -= instance.OnMenuPop;
+            @MenuPop.canceled -= instance.OnMenuPop;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -223,5 +252,6 @@ public partial class @InputAtionMap: IInputActionCollection2, IDisposable
         void OnMovement(InputAction.CallbackContext context);
         void OnRotate(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+        void OnMenuPop(InputAction.CallbackContext context);
     }
 }
---
+++ Assets/ProjectFolder/Scripts/UIScripts/IngameMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class IngameMenu : MonoBehaviour
+{
+    public AudioSource audioGEluidjeDingetje;
+    public GameObject SettingsMenu;
+    public bool PopupAan;
+
+    public void SettingsPopUp(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (!PopupAan)
+            {
+                PopupMenuOn();
+            }
+            else
+            {
+                PopupMenuOff();
+
+            }
+            PopupAan = !PopupAan;
+        }
+    }
+
+    public void PopupMenuOn()
+    {
+        SettingsMenu.SetActive(true);
+    }
+
+    public void PopupMenuOff()
+    {
+        SettingsMenu.SetActive(false);
+    }
+
+}
